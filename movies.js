ffa653a64550540ec5f514f1665b59f884618837
chris aquino <chris@radishmouse.com>
2019-04-25 13:06:06 -0400

    skeleton of movies db app

---
+++ movies.js
@@ -0,0 +1,23 @@
+const movies = {
+    "romantic comedies": {
+        "the exorcist": ["linda blair", "max von sydow", "ellen burstyn"],
+        "a nightmare on elm street": ["robert englund", "heather langenkamp", "johnny depp"],
+        "the shining": ["jack nicholson", "shelley duvall", "danny lloyd"],
+    },
+    "horror": {
+        "top gun": ["tom cruise", "val kilmer", "anthony edwards"],
+        "risky business": ["tom cruise", "rebecca de mornay", "bronson pinchot"],
+        "cocktail": ["tom cruise", "elizabeth shue", "bryan brown"]
+    },
+    "documentaries": {
+        "highlander": [
+            "christopher lambert", "sean connery", "clancy brown"
+        ],
+        "blade": [
+            "wesley snipes"
+        ],
+        "robocop": [
+            "peter weller", "nancy allen", "kurtwood smith"
+        ]
+    }
+}
